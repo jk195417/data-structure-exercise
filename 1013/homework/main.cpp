17d5f64877ae89be5138ea2061ec697d6892592f
楊竑昕 <jk195417@gmail.com>
2016-10-18 19:42:59 +0800

    c++

---
+++ 1013/homework/main.cpp
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#define MAX 100
+struct polynomial
+{
+	float coef;
+	int expon;
+
+};
+
+struct mypoly
+{
+	polynomial terms[MAX];
+	int total;
+
+	void ShowPoly();
+	mypoly Add(mypoly);
+	mypoly SingelMult(int);
+	int Lead_Exp();
+	void Attach(float,int);
+	void Remove(int);
+	mypoly Mult(mypoly);
+
+	mypoly(){
+
+		total = 0;
+
+	};
+	mypoly(char *filename)
+	{
+		FILE* fptr;
+		fptr =fopen(filename,"r");
+		total=0;
+		while(!feof(fptr))
+		{
+			fscanf(fptr,"%f %d",&terms[total].coef,&terms[total].expon);
+			total++;
+
+		}
+		fclose(fptr);
+	};
+};
+
+int main()
+{
+	mypoly A("a.txt");
+	mypoly B("b.txt");
+	int i;
+
+	A.ShowPoly();
+	B.ShowPoly();
+
+	A = A.SingelMult(3);
+	A.ShowPoly();
+
+
+	system("pause");
+	return 0;
+}
+
+void mypoly::ShowPoly()
+{
+
+	int i;
+	for(i=0;i<total;i++)
+	{
+		if(i<total-1)
+		{
+			printf("%.0fX^%d+",terms[i].coef,terms[i].expon);
+		}
+		else
+			printf("%.0fX^%d",terms[i].coef,terms[i].expon);
+	}
+	printf("€n");
+
+}
+
+mypoly mypoly::SingelMult(int c)
+{
+	mypoly D;
+	int i;
+
+	for(i=0;i<total;i++)
+	{
+		D.terms[i].coef = terms[i].coef * c ;
+		D.terms[i].expon = terms[i].expon ;
+		D.total++;
+	}
+	return D;
+}
